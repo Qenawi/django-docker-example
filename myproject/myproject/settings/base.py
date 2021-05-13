5d1009a4bcc8745b67597f81f8cb99c4e53f506e
Qenawi <aldmanmero@yahoo.com>
2021-05-13 06:50:48 -0400

    removed pip.env

--- myproject/myproject/settings/base.py
+++ myproject/myproject/settings/base.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DBBACKUP_STORAGE = "django.core.files.storage.FileSystemStorage"
 DBBACKUP_STORAGE_OPTIONS = {"location": "my/backup/dir/"}
 
